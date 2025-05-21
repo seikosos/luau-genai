835a331071bb251dcf9f93425e63091aa7f09b19
coolegang <dertfertde@gmail.com>
2025-05-21 19:15:04 +0200

    notice

--- example.lua
+++ example.lua
@@ -1,6 +1,6 @@
 local genai = require("genai")
 
 local client = genai.new("<YOUR_API_KEY>", "https://api.deepinfra.com/v1/openai/chat/completions")
-
+-- deep infra cuz openai completions support
 local chat = client:chat("meta-llama/Meta-Llama-3-8B-Instruct", { system_prompt = "" })
 print(chat:say("Hello, world!"))
