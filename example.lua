867c98931c4b1fc43dbc29357bcdd33bd9678c78
seikoso <75853142+seikosos@users.noreply.github.com>
2025-09-03 18:21:05 +0200

    Update example.lua

--- example.lua
+++ example.lua
@@ -4,3 +4,12 @@ local client = genai.new("<YOUR_API_KEY>", "https://generativelanguage.googleapi
 -- ngl gemini better
 local chat = client:chat("gemini-1.5-flash", { system_prompt = "" })
 print(chat:say("Hello, world!"))
+
+-- example for onemodule.lua
+
+local genai = loadstring(game:HttpGet("https://raw.githubusercontent.com/seikosos/luau-genai/refs/heads/main/onemodule.lua"))()
+
+local client = genai.new("<YOUR_API_KEY>", "https://generativelanguage.googleapis.com/v1beta/openai/")
+-- ngl gemini better
+local chat = client:chat("gemini-1.5-flash", { system_prompt = "" })
+print(chat:say("Hello, world!"))
