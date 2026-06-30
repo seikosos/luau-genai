134bdb906e0213321ebcd7ed120e49902753a778
seikosos <dertfertde@gmail.com>
2026-06-30 10:57:01 +0200

    update example to openrouter with free model

--- example.lua
+++ example.lua
@@ -1,17 +1,6 @@
--- example if you wanna require it for some reason
-
-local genai = require("genai")
-
-local client = genai.new("<YOUR_API_KEY>", "https://generativelanguage.googleapis.com/v1beta/openai/")
--- ngl gemini better
-local chat = client:chat("gemini-1.5-flash", { system_prompt = "" })
-print(chat:say("Hello, world!"))
-
--- example for onemodule.lua
-
 local genai = loadstring(game:HttpGet("https://raw.githubusercontent.com/seikosos/luau-genai/refs/heads/main/onemodule.lua"))()
 
-local client = genai.new("<YOUR_API_KEY>", "https://generativelanguage.googleapis.com/v1beta/openai/")
--- ngl gemini better
-local chat = client:chat("gemini-1.5-flash", { system_prompt = "" })
+local client = genai.new("<YOUR_API_KEY>", "https://openrouter.ai/api/v1/chat/completions")
+
+local chat = client:chat("cohere/north-mini-code:free", {system_prompt = ""})
 print(chat:say("Hello, world!"))
