674463df7a7d9bcfa3c9ae2864fdc0e2b5444753
seikosos <dertfertde@gmail.com>
2026-06-30 12:31:44 +0200

    update example to proper format

--- example.lua
+++ example.lua
@@ -1,6 +1,6 @@
 local genai = loadstring(game:HttpGet("https://raw.githubusercontent.com/seikosos/luau-genai/refs/heads/main/onemodule.lua"))()
 
-local client = genai.new("<YOUR_API_KEY>", "https://openrouter.ai/api/v1/chat/completions")
+local client = genai.new("<YOUR_API_KEY>", "openai::https://openrouter.ai/api/v1/chat/completions")
 
 local chat = client:chat("cohere/north-mini-code:free", {system_prompt = ""})
 print(chat:say("Hello, world!"))
