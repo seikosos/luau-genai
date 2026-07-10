9839f83754a1320d97a0ba5c63dd35e7100e26be
seikosos <dertfertde@gmail.com>
2026-07-10 15:31:05 +0200

    dsfsdfsdfsdfsd

--- onemodule.lua
+++ onemodule.lua
@@ -448,6 +448,7 @@ end
 ---@return number input_tokens
 ---@return number output_tokens
 function openai.extract_response_data(response)
+	if not response or not response.choices then return "INVALID???", 0 , 0 end
 	local choice = table.remove(response.choices, 1)
 	local reply = choice and choice.message and choice.message.content or "INVALID RESPONSE"
 	local input_tokens = response.usage and response.usage.prompt_tokens or 0
@@ -693,20 +694,6 @@ local ObjectTree = {
         },
         {
             {
-                4,
-                2,
-                {
-                    "genai"
-                }
-            },
-            {
-                8,
-                2,
-                {
-                    "utils"
-                }
-            },
-            {
                 2,
                 2,
                 {
@@ -723,6 +710,20 @@ local ObjectTree = {
                 }
             },
             {
+                8,
+                2,
+                {
+                    "utils"
+                }
+            },
+            {
+                4,
+                2,
+                {
+                    "genai"
+                }
+            },
+            {
                 5,
                 2,
                 {
@@ -730,17 +731,17 @@ local ObjectTree = {
                 },
                 {
                     {
-                        6,
-                        2,
-                        {
-                            "anthropic"
-                        }
-                    },
-                    {
                         7,
                         2,
                         {
                             "openai"
+                        }
+                    },
+                    {
+                        6,
+                        2,
+                        {
+                            "anthropic"
                         }
                     }
                 }
@@ -758,7 +759,7 @@ local LineOffsets = {
     190,
     199,
     366,
-    550
+    551
 }
 
 -- Misc AOT variable imports
